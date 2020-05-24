23502ada256dd85d6890870d19866aad3b255f79
Abhimanyu Kumar <58354441+ak22011996@users.noreply.github.com>
2020-05-24 22:40:45 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,19 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet,TextInput,TouchableOpacity} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hi</Text>
-    </View>
-  );
+
+class App extends React.Component {
+
+        state = {
+            value: 'Show',
+            isPasswordVisible:true
+        }
+
+        toggle = () => {
+            const newValue = this.state.isPasswordVisible;
+            if (newValue == true) {
+                this.setState({
+                    isPasswordVisible: false,
+                    value: 'Hide'
+                })
+            } else {
+                this.setState({
+                    isPasswordVisible: true,
+                    value: 'Show'
+                })
+            }
+        }
+	
+render(){
+  return(
+  		<View style={styles.container}>
+		    <TextInput secureTextEntry={this.state.isPasswordVisible}
+		    	style={{backgroundColor: '#a7a6a9',height:50,width:400,fontSize:30}}>
+		    </TextInput>
+		    <TouchableOpacity>
+		        <Text style={{fontSize:40}} onPress={this.toggle}>{this.state.value}</Text>
+		    </TouchableOpacity>
+		</View>
+	   )
+ }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		marginTop:100,
+		flex:1,
+		alignItems:'center'
+	}
 });
+
+export default App;
